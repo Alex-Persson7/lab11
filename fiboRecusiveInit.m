e31c7da0876105a71bfaa970ab0c11a355f0fdfe
Alex-Persson7 <alex.persson1@hotmail.com>
2021-04-26 15:53:35 -0500

    merged version of the fibonacci sequence

--- fiboRecusiveInit.m
+++ fiboRecusiveInit.m
@@ -2,6 +2,6 @@ function [f] = fiboRecusiveInit(n)
 global count;
  count = 0;
 f = fiboRecusive(n);
-fprintf('It took %d function calls to calculate that the %dth Fibonacci number is %d.\n',count, n, f);
+fprintf('It took %d function calls to calculate that the %dth Fibonacci number is %d.\n',count, n, f);%test
 
 end
